d29e3a95c687f5a64630307e7d4ab93d87b10e01
codeboyzhou <imzhouchen@gmail.com>
2025-06-09 12:53:45 +0800

    test(common): Add unit tests for BufferQueue

--- src/main/java/com/github/codeboyzhou/mcp/declarative/exception/McpServerException.java
+++ src/main/java/com/github/codeboyzhou/mcp/declarative/exception/McpServerException.java
@@ -2,10 +2,6 @@ package com.github.codeboyzhou.mcp.declarative.exception;
 
 public class McpServerException extends RuntimeException {
 
-    public McpServerException(String message) {
-        super(message);
-    }
-
     public McpServerException(String message, Throwable cause) {
         super(message, cause);
     }
